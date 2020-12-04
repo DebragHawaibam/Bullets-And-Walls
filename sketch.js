6d74730590df5fdabd0a4eadd6949b9b91341319
teach me how to code and how to hacked <70337208+hawaibamdebrag@users.noreply.github.com>
2020-12-04 11:36:25 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+var wall,thickness;
+var bullet,speed,weight;
+
+
+function setup() {
+  createCanvas(1600,400);
+
+  speed=random(223,321)
+  weight=random(30,52)
+  thickness=random(22,83)
+
+  bullet=createSprite(50,200,50,20);
+  bullet.shapeColor = color("gray")
+  bullet.velocityX=speed;
+
+  wall=createSprite(1200,200,thickness,200)
+}
+
+function draw() {
+  background("aqua");  
+
+ if(hasCollided(bullet, wall)){
+   bullet.velocityX=0
+   var damage=0.5 * weight * speed * speed/(thickness * thickness * thickness);
+
+   if(damage>10){
+     wall.shapeColor=color(255,0,0)
+   }
+
+   if(damage<10){
+     wall.shapeColor=color(0,255,0)
+   }
+ }
+
+  drawSprites();
+}
+
+function hasCollided(lbullet,lwall) {
+  bulletRightEdge=lbullet.x+lbullet.width
+  wallLeftEdge=lwall.x;
+  if(bulletRightEdge>=wallLeftEdge){
+    return true
+  }
+  return false
+}
